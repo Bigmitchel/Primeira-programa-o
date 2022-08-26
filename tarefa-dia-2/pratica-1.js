9040d50bb82aa4d37d4db0fa0c8e29e96f3eddb1
Michel Junior <michel.alessandro98@hotmail.com>
2022-08-25 22:28:29 -0300

    Correção definitiva Tarefa 2 - Variáveis

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,15 @@
 let nome
-let email
+nome = "Michel Junior"
+console.log(nome)
+let email 
+email = "michel.alessandro98@hotmail.com"
+console.log(email)
 let profissão
+profissão = "autonomo"
+console.log(profissão)
 let idade
-let Viagem
+idade = 24
+console.log(idade)
+let viagem
+viagem = "San Francisco"
+console.log(viagem)
