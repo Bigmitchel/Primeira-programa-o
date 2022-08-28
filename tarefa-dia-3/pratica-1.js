ebc3c63829d6e55357483ca73cc455f0fa8a018b
Michel Junior <michel.alessandro98@hotmail.com>
2022-08-28 12:03:07 -0300

    Pratica 1 (Dia 3) Concluída. q

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,10 @@
+//1° Tarefa da atividade 3:
+//Fazer sequências do número 1 ao 9 com Loop.
+
+let n = 1 // A variável.
+
+while (n <= 9){
+    console.log(n) // 9 vezes
+    n = n + 1 // Aqui, coloca o seguinte comando (variavel = variavel + 1) para o processador somar até formar 9.
+}
+// Loop para fazer repetição até 9 vezes.
